73e3b754b6d97b7e321cac4d1c1b794feef8aaf4
zerovolk <sozikin.artyom@yandex.ru>
2022-06-23 21:32:28 +0500

    init

---
+++ src/components/PostForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+  <form @submit.prevent>
+    <h4>Название поста</h4>
+    <my-input
+        v-model="post.title"
+        type="text"
+        placeholder="Название"
+    />
+    <my-input
+        v-model="post.body"
+        type="text"
+        placeholder="Описание"
+    />
+    <my-button
+        @click="createPost"
+        style="align-self: flex-end; margin-top: 15px;"
+    >
+      Создать
+    </my-button>
+  </form>
+  </div>
+</template>
+
+<script>
+import MyInput from "@/components/UI/MyInput";
+export default {
+  components: {MyInput},
+  data() {
+    return {
+      post: {
+        title: '',
+        body: '',
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: '',
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'my-button'
+}
+</script>
+
+<style scoped>
+.btn {
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  transition: background 600ms ease;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: teal;
+  color: aliceblue;
+}
+</style>
