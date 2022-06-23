4bc788003efd74fcf7c0b1a96ecd614dc263d1ac
zerovolk <sozikin.artyom@yandex.ru>
2022-06-23 21:32:28 +0500

    init

---
+++ src/components/UI/MyDialog.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="dialog" v-if="show" @click.stop="hideDialog">
+    <div @click.stop class="dialog__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-dialog',
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    hideDialog() {
+      this.$emit('update:show', false)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dialog {
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
+  z-index: 10;
+}
+
+
+.dialog__content {
+  margin: auto;
+  background: #fff;
+  border-radius: 12px;
+  min-height: 50px;
+  min-width: 300px;
+  padding: 20px;
+}
+</style>
---
+++ src/components/UI/index.js
@@ -0,0 +1,11 @@
+import myButton from "@/components/UI/MyButton";
+import myInput from "@/components/UI/MyInput";
+import myDialog from "@/components/UI/MyDialog";
+import mySelect from "@/components/UI/MySelect";
+
+export default [
+    myButton,
+    myInput,
+    myDialog,
+    mySelect,
+]
