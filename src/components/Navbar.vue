b323cf91c4f2e2df8a82dfd943cc82c583ceeb0d
zerovolk <sozikin.artyom@yandex.ru>
2022-06-24 23:28:58 +0500

    added many options, like autoload posts, navigation bar, single-page functions in the app

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')">Social Posts</div>
+    <div class="navbar__btns">
+      <my-button @click="$router.push('/posts')">Посты</my-button>
+      <my-button @click="$router.push('/about')" style="margin-left: 15px;">О сайте</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+
+
+}
+</script>
+
+<style scoped>
+.navbar {
+  height: 50px;
+  background-color: lightgrey;
+  box-shadow: 2px 2px 4px grey;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+
+.navbar__btns {
+  margin-left: auto;
+}
+</style>
